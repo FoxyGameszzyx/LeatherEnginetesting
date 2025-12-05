3277c72d109d8c601867a33f29decbca9b192c61
FoxyGameszzyx <jobanygonzalez962@gmail.com>
2025-12-04 20:19:04 -0800

    Update FlxText.hx

--- source/flixel/text/FlxText.hx
+++ source/flixel/text/FlxText.hx
@@ -268,94 +268,94 @@ import openfl.utils.AssetType;
 	 * @param   input   The text you want to format
 	 * @param   rules   `FlxTextFormat`s to selectively apply, paired with marker strings
 	 */
-	public function applyMarkup(input:String, rules:Array<FlxTextFormatMarkerPair>):FlxText {
-		if (rules == null || rules.length == 0)
-			return this; // there's no point in running the big loop
+	//public function applyMarkup(input:String, rules:Array<FlxTextFormatMarkerPair>):FlxText {
+		//if (rules == null || rules.length == 0)
+			//return this; // there's no point in running the big loop
 
-		clearFormats(); // start with default formatting
+		//clearFormats(); // start with default formatting
 
-		var rangeStarts:Array<Int> = [];
-		var rangeEnds:Array<Int> = [];
-		var rulesToApply:Array<FlxTextFormatMarkerPair> = [];
+		//var rangeStarts:Array<Int> = [];
+		//var rangeEnds:Array<Int> = [];
+		//var rulesToApply:Array<FlxTextFormatMarkerPair> = [];
 
-		var i:Int = 0;
-		for (rule in rules) {
-			if (rule.marker == null || rule.format == null)
-				continue;
+		//var i:Int = 0;
+		//for (rule in rules) {
+			//if (rule.marker == null || rule.format == null)
+				//continue;
 
-			var start:Bool = false;
-			var markerLength:Int = rule.marker.uLength();
+			//var start:Bool = false;
+			//var markerLength:Int = rule.marker.uLength();
 
-			if (!input.contains(rule.marker))
-				continue; // marker not present
+			//if (!input.contains(rule.marker))
+				//continue; // marker not present
 
 			// inspect each character
-			for (charIndex in 0...input.uLength()) {
-				if (!input.uSub(charIndex, markerLength).uEquals(rule.marker))
-					continue; // it's not one of the markers
+			//for (charIndex in 0...input.uLength()) {
+				//if (!input.uSub(charIndex, markerLength).uEquals(rule.marker))
+					//continue; // it's not one of the markers
 
-				if (start) {
-					start = false;
-					rangeEnds.push(charIndex); // end a format block
-				} else // we're outside of a format block
-				{
-					start = true; // start a format block
-					rangeStarts.push(charIndex);
-					rulesToApply.push(rule);
-				}
-			}
+				//if (start) {
+					//start = false;
+					//rangeEnds.push(charIndex); // end a format block
+				//} else // we're outside of a format block
+				//{
+				//	start = true; // start a format block
+					//rangeStarts.push(charIndex);
+					//rulesToApply.push(rule);
+				//}
+			//}
 
-			if (start) {
+			//if (start) {
 				// we ended with an unclosed block, mark it as infinite
-				rangeEnds.push(-1);
-			}
+				//rangeEnds.push(-1);
+			//}
 
-			i++;
-		}
+			//i++;
+		//}
 
 		// Remove all of the markers in the string
-		for (rule in rules)
-			input = input.remove(rule.marker);
+		//for (rule in rules)
+			//input = input.remove(rule.marker);
 
 		// Adjust all the ranges to reflect the removed markers
-		for (i in 0...rangeStarts.length) {
+		//for (i in 0...rangeStarts.length) {
 			// Consider each range start
-			var delIndex:Int = rangeStarts[i];
-			var markerLength:Int = rulesToApply[i].marker.uLength();
+		//	var delIndex:Int = rangeStarts[i];
+			//var markerLength:Int = rulesToApply[i].marker.uLength();
 
 			// Any start or end index that is HIGHER than this must be subtracted by one markerLength
-			for (j in 0...rangeStarts.length) {
-				if (rangeStarts[j] > delIndex) {
-					rangeStarts[j] -= markerLength;
-				}
-				if (rangeEnds[j] > delIndex) {
-					rangeEnds[j] -= markerLength;
-				}
-			}
+			//for (j in 0...rangeStarts.length) {
+			//	if (rangeStarts[j] > delIndex) {
+					//rangeStarts[j] -= markerLength;
+				//}
+				//if (rangeEnds[j] > delIndex) {
+					//rangeEnds[j] -= markerLength;
+				//}
+			//}
 
 			// Consider each range end
-			delIndex = rangeEnds[i];
+			//delIndex = rangeEnds[i];
 
 			// Any start or end index that is HIGHER than this must be subtracted by one markerLength
-			for (j in 0...rangeStarts.length) {
-				if (rangeStarts[j] > delIndex) {
-					rangeStarts[j] -= markerLength;
-				}
-				if (rangeEnds[j] > delIndex) {
-					rangeEnds[j] -= markerLength;
-				}
-			}
-		}
+			//for (j in 0...rangeStarts.length) {
+				//if (rangeStarts[j] > delIndex) {
+					//rangeStarts[j] -= markerLength;
+				//}
+				//if (rangeEnds[j] > delIndex) {
+					//rangeEnds[j] -= markerLength;
+				//}
+			//}
+		//}
 
 		// Apply the new text
-		text = input;
+		//text = input;
 
 		// Apply each format selectively to the given range
-		for (i in 0...rangeStarts.length)
-			addFormat(rulesToApply[i].format, rangeStarts[i], rangeEnds[i]);
+		//for (i in 0...rangeStarts.length)
+			//addFormat(rulesToApply[i].format, rangeStarts[i], rangeEnds[i]);
 
-		return this;
-	}
+		//return this;
+	//}
 
 	/**
 	 * Adds another format to this `FlxText`
